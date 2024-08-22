3409b456e847205544f8c0cdb06934584c233f2d
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2024-08-22 21:43:49 +0530

    Adding c2 assignment3

---
+++ src/Ultimate_Java_Masterclass/assignment3/question1.java
@@ -0,0 +1,55 @@
+package Ultimate_Java_Masterclass.assignment3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class question1 {
+
+    public static void main(String[] args) {
+    try
+
+    {
+        readFile("test.txt");
+    }
+    catch(IOException e)
+    {
+        System.out.println(e);
+    }
+
+    try{
+        throw new IOException("Manual");
+    }
+    catch (IOException e)
+    {
+        System.out.println(e);
+    }
+
+}
+    public static void readFile(String file) throws IOException {
+        BufferedReader br = null;
+        try {
+             br = new BufferedReader(new FileReader(file));
+             String line;
+                while ((line = br.readLine()) != null)
+                 {
+                System.out.println(line);
+                }
+              }
+            catch (IOException e)
+            {
+                System.out.println(e);
+                throw e;
+            }
+        finally {
+            try{
+                if (br != null){
+                    br.close();}
+            }
+            catch (IOException e) {
+                System.out.println(e);
+            }
+            System.out.println("Finally executed");
+        }
+    }
+}
